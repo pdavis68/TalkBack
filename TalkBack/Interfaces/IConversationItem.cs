250565ea37f4c1ea6b210783f21e3cab0e1fdee3
Pete <pdavis@westmonroepartners.com>
2024-06-26 18:47:08 -0500

    Made properties public

--- TalkBack/Interfaces/IConversationItem.cs
+++ TalkBack/Interfaces/IConversationItem.cs
@@ -2,6 +2,6 @@
 
 public class IConversationItem
 {
-    string User { get; set; }  = string.Empty;
-    string Assistant { get; set; } = string.Empty;
+    public string User { get; set; }  = string.Empty;
+    public string Assistant { get; set; } = string.Empty;
 }

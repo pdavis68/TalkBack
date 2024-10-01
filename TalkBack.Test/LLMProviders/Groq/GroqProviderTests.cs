2f6513f4eec65dd5025eaba1fc04d6ece050f390
Pete <pdavis@westmonroepartners.com>
2024-10-01 12:57:25 -0500

    test: Add GroqProviderTests

--- TalkBack.Test/LLMProviders/Groq/GroqProviderTests.cs
+++ TalkBack.Test/LLMProviders/Groq/GroqProviderTests.cs
@@ -1,10 +1,7 @@
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using System.Net;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
-using System.Text.Json;
-using TalkBack.Exceptions;
 using TalkBack.Interfaces;
 using TalkBack.LLMProviders.Groq;
 
